4dde04e317e6bbdcaaf3155b89d59d403f83c249
abhishek372 <abhishekkumar111109@gmail.com>
2022-09-05 21:08:12 +0530

    Changes in profile.js

--- js/profile_page.js
+++ js/profile_page.js
@@ -1,3 +1,7 @@
+
+
+
+
 //smriti
 var profilePic = document.querySelector(".profile-pic");
 var fullName = document.querySelector(".full-name");
@@ -12,117 +16,102 @@ fetch("https://server.shehnayi.in/api/v2/home", {
     return ravi.json();
   })
   .then((data) => {
-    console.log(data);
+    console.log(data.foundUser);
+    // Abhishek
+document.querySelector(".apifirstname-am").setAttribute('value', data.foundUser.firstname);
+document.querySelector(".apilastname-am").setAttribute('value', data.foundUser.lastname);
+document.querySelector(".apidays-am").setAttribute('value', data.foundUser.birthday.day);
+document.querySelector(".apimonth-am").setAttribute('value', data.foundUser.birthday.month);
+document.querySelector(".apiyear-am").setAttribute('value', data.foundUser.birthday.year);
+document.querySelector(".apiheight-am").value = data.foundUser.height;
+document.querySelector(".apimarital-am").value = data.foundUser.maritalStatus;
+document.querySelector(".apioccupation-am").value = data.foundUser.designation;
+document.querySelector(".apicity-am").setAttribute('value', data.foundUser.city);
+document.querySelector(".apistate-am").setAttribute('value', data.foundUser.state);
+
+document.querySelector(".apireligion-am").setAttribute('value', data.foundUser.religion);
+document.querySelector(".apisubcaste-am").setAttribute('value', data.foundUser.caste);
+document.querySelector(".apimanglik-am").value = data.foundUser.manglik;
+
+document.querySelector(".apiqualification-am").value = data.foundUser.heighestQualification;
+document.querySelector(".apicompany-am").setAttribute('value', data.foundUser.company);
+document.querySelector(".apiannualincome-am").value = data.foundUser.AnnualIncome;
+
+// document.querySelector(".apieating-am").value = data.foundUser.eating;
+// document.querySelector(".apidrinking-am").value = data.foundUser.drinking;
+// document.querySelector(".apismoking-am").value = data.foundUser.smoking;
+
+// document.querySelector(".apifatherstatus-am").value = data.foundUser.fatherStatus;
+// document.querySelector(".apimotherstatus-am").value = data.foundUser.motherStatus;
+// document.querySelector(".apinoofbrothers-am").setAttribute('value', data.foundUser.brothers);
+// document.querySelector(".apinoofsisters-am").setAttribute('value', data.foundUser.sisters);
+
+// document.querySelector(".apifromage-am").setAttribute('value', data.foundUser.userPreference.fromAge);
+// document.querySelector(".apitoage-am").setAttribute('value', data.foundUser.userPreference.toAge);
+// document.querySelector(".apifromheight-am").setAttribute('value', data.foundUser.userPreference.fromHeight);
+// document.querySelector(".apitoheight-am").setAttribute('value', data.foundUser.userPreference.toHeight);
+// document.querySelector(".apiparmarital-am").value = data.foundUser.userPreference.maritalStatus;
+// document.querySelector(".apiparreligion-am").setAttribute('value', data.foundUser.userPreference.religion);
+// document.querySelector(".apiparcommunity-am").setAttribute('value', data.foundUser.userPreference.maritalStatus);
+
+// document.querySelector(".apiparcity-am").setAttribute('value', data.foundUser.userPreference.city);
+// document.querySelector(".apiparstate-am").setAttribute('value', data.foundUser.userPreference.state);
+// document.querySelector(".apiparcountry-am").setAttribute('value', data.foundUser.userPreference.country);
+
+// document.querySelector(".apiparqualification").setAttribute('value', data.foundUser.userPreference.heighestQualification);
+// document.querySelector(".apiparcompany-am").setAttribute('value', data.foundUser.userPreference.company);
+// document.querySelector(".apiparannualincome-am").value = data.foundUser.userPreference.AnnualIncome;
+
+
+// localStorage.setItem("am" , document.querySelector(".apiheight-am").value);
+
     profilePic.innerHTML = `<img id="myImg" src="https://server.shehnayi.in/${data.foundUser.profilePicture}" alt="" />
             <i id="add" class="ri-add-circle-line"></i>`;
 
     fullName.textContent = `${data.foundUser.firstname} ${data.foundUser.lastname}`;
-    document.querySelector(
-      ".apippage"
-    ).innerHTML = `${data.foundUser.age} yrs,${data.foundUser.height},${data.foundUser.city}`;
+    document.querySelector(".apippage").innerHTML = `${data.foundUser.age} yrs,${data.foundUser.height}`;
     document.querySelector(".apippcaste").innerHTML = `${data.foundUser.caste}`;
-    document.querySelector(
-      ".apippsubcaste"
-    ).innerHTML = `${data.foundUser.subCaste}`;
-    document.querySelector(
-      ".apippmaritalstatus"
-    ).innerHTML = `${data.foundUser.maritalStatus}`;
-    document.querySelector(
-      ".apippplace"
-    ).innerHTML = `Lives in ${data.foundUser.city}, ${data.foundUser.state}`;
-    document.querySelector(
-      ".apippdesignation"
-    ).innerHTML = `${data.foundUser.designation}`;
-    document.querySelector(
-      ".apippdesc"
-    ).innerHTML = `${data.foundUser.profileSummary}`;
-    document.querySelector(
-      ".apippusername"
-    ).innerHTML = `${data.foundUser.firstname} ${data.foundUser.lastname}`;
-    document.querySelector(
-      ".apippbday"
-    ).innerHTML = `${data.foundUser.birthday.day}/${data.foundUser.birthday.month}/${data.foundUser.birthday.year}`;
+    document.querySelector(".apippsubcaste").innerHTML = `${data.foundUser.subCaste}`;
+    document.querySelector(".apippmaritalstatus").innerHTML = `${data.foundUser.maritalStatus}`;
+    document.querySelector(".apippplace").innerHTML = `Lives in ${data.foundUser.city}, ${data.foundUser.state}`;
+    document.querySelector(".apippdesignation").innerHTML = `${data.foundUser.designation}`;
+    document.querySelector(".apippdesc").innerHTML = `${data.foundUser.profileSummary}`;
+    document.querySelector(".apippusername").innerHTML = `${data.foundUser.firstname} ${data.foundUser.lastname}`;
+    document.querySelector(".apippbday").innerHTML = `${data.foundUser.birthday.day}/${data.foundUser.birthday.month}/${data.foundUser.birthday.year}`;
     document.querySelector(".apipph").innerHTML = `${data.foundUser.height}`;
-    document.querySelector(
-      ".apippms"
-    ).innerHTML = `${data.foundUser.maritalStatus}`;
-    document.querySelector(
-      ".apippdsg"
-    ).innerHTML = `${data.foundUser.designation}`;
-    document.querySelector(
-      ".apippp"
-    ).innerHTML = `${data.foundUser.city}, ${data.foundUser.state}`;
+    document.querySelector(".apippms").innerHTML = `${data.foundUser.maritalStatus}`;
+    document.querySelector(".apippdsg").innerHTML = `${data.foundUser.designation}`;
+    document.querySelector(".apippp").innerHTML = `${data.foundUser.city}, ${data.foundUser.state}`;
     document.querySelector(".apipprg").innerHTML = `${data.foundUser.religion}`;
     document.querySelector(".apippcs").innerHTML = `${data.foundUser.caste}`;
-    document.querySelector(
-      ".apippmangliks"
-    ).innerHTML = `${data.foundUser.manglik}`;
-    document.querySelector(
-      ".apippuhq"
-    ).innerHTML = `${data.foundUser.heighestQualification}`;
-    document.querySelector(
-      ".apippucompany"
-    ).innerHTML = `${data.foundUser.company}`;
-    document.querySelector(
-      ".apippuannum"
-    ).innerHTML = `${data.foundUser.AnnualIncome}`;
+    document.querySelector(".apippmangliks").innerHTML = `${data.foundUser.manglik}`;
+    document.querySelector(".apippuhq").innerHTML = `${data.foundUser.heighestQualification}`;
+    document.querySelector(".apippucompany").innerHTML = `${data.foundUser.company}`;
+    document.querySelector(".apippuannum").innerHTML = `${data.foundUser.AnnualIncome}`;
     document.querySelector(".apippeat").innerHTML = `${data.foundUser.eating}`;
-    document.querySelector(
-      ".apippdrink"
-    ).innerHTML = `${data.foundUser.drinking}`;
-    document.querySelector(
-      ".apippsmoke"
-    ).innerHTML = `${data.foundUser.smoking}`;
-    document.querySelector(
-      ".apippfr"
-    ).innerHTML = `${data.foundUser.fatherStatus}`;
-    document.querySelector(
-      ".apippmr"
-    ).innerHTML = `${data.foundUser.motherStatus}`;
-    document.querySelector(
-      ".apippnbr"
-    ).innerHTML = `${data.foundUser.brothers}`;
+    document.querySelector(".apippdrink").innerHTML = `${data.foundUser.drinking}`;
+    document.querySelector(".apippsmoke").innerHTML = `${data.foundUser.smoking}`;
+    document.querySelector(".apippfr").innerHTML = `${data.foundUser.fatherStatus}`;
+    document.querySelector(".apippmr").innerHTML = `${data.foundUser.motherStatus}`;
+    document.querySelector(".apippnbr").innerHTML = `${data.foundUser.brothers}`;
     document.querySelector(".apippns").innerHTML = `${data.foundUser.sisters}`;
-    document.querySelector(
-      ".apipppage"
-    ).innerHTML = `${data.foundUser.userPreference.fromAge} to ${data.foundUser.userPreference.toAge} yrs`;
-    document.querySelector(
-      ".apippph"
-    ).innerHTML = `${data.foundUser.userPreference.fromHeight} to ${data.foundUser.userPreference.toHeight}`;
-    document.querySelector(
-      ".apipppms"
-    ).innerHTML = `${data.foundUser.userPreference.maritalStatus}`;
-    document.querySelector(
-      ".apippprg"
-    ).innerHTML = `${data.foundUser.userPreference.religion}`;
-    document.querySelector(
-      ".apipppcs"
-    ).innerHTML = `${data.foundUser.userPreference.caste}`;
-    document.querySelector(
-      ".apipppsc"
-    ).innerHTML = `${data.foundUser.userPreference.subCaste}`;
-    document.querySelector(
-      ".apipppcity"
-    ).innerHTML = `${data.foundUser.userPreference.city}`;
-    document.querySelector(
-      ".apipppstate"
-    ).innerHTML = `${data.foundUser.userPreference.state}`;
-    document.querySelector(
-      ".apipppcountry"
-    ).innerHTML = `${data.foundUser.userPreference.country}`;
-    document.querySelector(
-      ".apippphq"
-    ).innerHTML = `${data.foundUser.userPreference.heighestQualification}`;
-    document.querySelector(
-      ".apipppcompany"
-    ).innerHTML = `${data.foundUser.userPreference.company}`;
-    document.querySelector(
-      ".apipppannum"
-    ).innerHTML = `${data.foundUser.userPreference.AnnualIncome}`;
+    document.querySelector(".apipppage").innerHTML = `${data.foundUser.userPreference.fromAge} to ${data.foundUser.userPreference.toAge} yrs`;
+    document.querySelector(".apippph").innerHTML = `${data.foundUser.userPreference.fromHeight} to ${data.foundUser.userPreference.toHeight}`;
+    document.querySelector(".apipppms").innerHTML = `${data.foundUser.userPreference.maritalStatus}`;
+    document.querySelector(".apippprg").innerHTML = `${data.foundUser.userPreference.religion}`;
+    document.querySelector(".apipppcs").innerHTML = `${data.foundUser.userPreference.caste}`;
+    document.querySelector(".apipppsc").innerHTML = `${data.foundUser.userPreference.subCaste}`;
+    document.querySelector(".apipppcity").innerHTML = `${data.foundUser.userPreference.city}`;
+    document.querySelector(".apipppstate").innerHTML = `${data.foundUser.userPreference.state}`;
+    document.querySelector(".apipppcountry").innerHTML = `${data.foundUser.userPreference.country}`;
+    document.querySelector(".apippphq").innerHTML = `${data.foundUser.userPreference.heighestQualification}`;
+    document.querySelector(".apipppcompany").innerHTML = `${data.foundUser.userPreference.company}`;
+    document.querySelector(".apipppannum").innerHTML = `${data.foundUser.userPreference.AnnualIncome}`;
   })
   .catch((err) => {
     console.log(err);
   });
+
 
 // const add = document.querySelector("#add");
 // const addimages = document.querySelector("#addimages");
@@ -137,11 +126,11 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
+// img.onclick = function () {
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// };
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -150,6 +139,7 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
   modal.style.display = "none";
 };
+
 
 // Edit button form
 var editbtn = document.querySelector(".edit-txt");
@@ -174,156 +164,117 @@ editoverlay.addEventListener("click", function () {
 });
 
 // Close buttons
-document
-  .querySelector("#inner-form-1 #close-icon-1")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-1 #close-icon-1").addEventListener("click", function () {
+  popup();
+});
+
+document.querySelector("#inner-form-2 #close-icon-2").addEventListener("click", function () {
+    popup();
+});
+
+document.querySelector("#inner-form-3 #close-icon-3").addEventListener("click", function () {
+    popup();
+});
+
+document.querySelector("#inner-form-4 #close-icon-4").addEventListener("click", function () {
+    popup();
+});
+
+document.querySelector("#inner-form-5 #close-icon-5").addEventListener("click", function () {
     popup();
   });
 
-document
-  .querySelector("#inner-form-2 #close-icon-2")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-6 #close-icon-6").addEventListener("click", function () {
     popup();
   });
 
-document
-  .querySelector("#inner-form-3 #close-icon-3")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-7 #close-icon-7").addEventListener("click", function () {
     popup();
   });
 
-document
-  .querySelector("#inner-form-4 #close-icon-4")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-8 #close-icon-8").addEventListener("click", function () {
     popup();
   });
 
-document
-  .querySelector("#inner-form-5 #close-icon-5")
-  .addEventListener("click", function () {
-    popup();
-  });
+var textinput = document.querySelectorAll(".text-input");
 
-document
-  .querySelector("#inner-form-6 #close-icon-6")
-  .addEventListener("click", function () {
-    popup();
-  });
-
-document
-  .querySelector("#inner-form-7 #close-icon-7")
-  .addEventListener("click", function () {
-    popup();
-  });
-
-document
-  .querySelector("#inner-form-8 #close-icon-8")
-  .addEventListener("click", function () {
-    popup();
-  });
 
 // Form slides
-document
-  .querySelector("#inner-form-1 .nexti-btn")
-  .addEventListener("click", function (event) {
-    document.querySelector("#inner-form-1").style.display = "none";
-    document.querySelector("#inner-form-2").style.display = "initial";
-    event.preventDefault();
+document.querySelector("#inner-form-1 .nexti-btn").addEventListener("click", function (event) {
+      document.querySelector("#inner-form-1").style.display = "none";
+      document.querySelector("#inner-form-2").style.display = "initial";
+      event.preventDefault();
   });
 
-document
-  .querySelector("#inner-form-2 .flex-div .previ-btn")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-2 .flex-div .previ-btn").addEventListener("click", function () {
     document.querySelector("#inner-form-2").style.display = "none";
     document.querySelector("#inner-form-1").style.display = "initial";
   });
 
-document
-  .querySelector("#inner-form-2 .nexti-btn")
-  .addEventListener("click", function (event) {
+document.querySelector("#inner-form-2 .nexti-btn").addEventListener("click", function (event) {
     document.querySelector("#inner-form-2").style.display = "none";
     document.querySelector("#inner-form-3").style.display = "initial";
     event.preventDefault();
   });
 
-document
-  .querySelector("#inner-form-3 .previ-btn")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-3 .previ-btn").addEventListener("click", function () {
     document.querySelector("#inner-form-3").style.display = "none";
     document.querySelector("#inner-form-2").style.display = "initial";
   });
 
-document
-  .querySelector("#inner-form-3 .nexti-btn")
-  .addEventListener("click", function (event) {
+document.querySelector("#inner-form-3 .nexti-btn").addEventListener("click", function (event) {
     document.querySelector("#inner-form-3").style.display = "none";
     document.querySelector("#inner-form-4").style.display = "initial";
     event.preventDefault();
   });
 
-document
-  .querySelector("#inner-form-4 .previ-btn")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-4 .previ-btn").addEventListener("click", function () {
     document.querySelector("#inner-form-4").style.display = "none";
     document.querySelector("#inner-form-3").style.display = "initial";
   });
 
-document
-  .querySelector("#inner-form-4 .nexti-btn")
-  .addEventListener("click", function (event) {
+document.querySelector("#inner-form-4 .nexti-btn").addEventListener("click", function (event) {
     document.querySelector("#inner-form-4").style.display = "none";
     document.querySelector("#inner-form-5").style.display = "initial";
     event.preventDefault();
   });
 
-document
-  .querySelector("#inner-form-5 .previ-btn")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-5 .previ-btn").addEventListener("click", function () {
     document.querySelector("#inner-form-5").style.display = "none";
     document.querySelector("#inner-form-4").style.display = "initial";
   });
 
-document
-  .querySelector("#inner-form-5 .nexti-btn")
-  .addEventListener("click", function (event) {
+document.querySelector("#inner-form-5 .nexti-btn").addEventListener("click", function (event) {
     document.querySelector("#inner-form-5").style.display = "none";
     document.querySelector("#inner-form-6").style.display = "initial";
     event.preventDefault();
   });
 
-document
-  .querySelector("#inner-form-6 .previ-btn")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-6 .previ-btn").addEventListener("click", function () {
     document.querySelector("#inner-form-6").style.display = "none";
     document.querySelector("#inner-form-5").style.display = "initial";
   });
 
-document
-  .querySelector("#inner-form-6 .nexti-btn")
-  .addEventListener("click", function (event) {
+document.querySelector("#inner-form-6 .nexti-btn").addEventListener("click", function (event) {
     document.querySelector("#inner-form-6").style.display = "none";
     document.querySelector("#inner-form-7").style.display = "initial";
     event.preventDefault();
   });
 
-document
-  .querySelector("#inner-form-7 .previ-btn")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-7 .previ-btn").addEventListener("click", function () {
     document.querySelector("#inner-form-7").style.display = "none";
     document.querySelector("#inner-form-6").style.display = "initial";
   });
 
-document
-  .querySelector("#inner-form-7 .nexti-btn")
-  .addEventListener("click", function (event) {
+document.querySelector("#inner-form-7 .nexti-btn").addEventListener("click", function (event) {
     document.querySelector("#inner-form-7").style.display = "none";
     document.querySelector("#inner-form-8").style.display = "initial";
     event.preventDefault();
   });
 
-document
-  .querySelector("#inner-form-8 .previ-btn")
-  .addEventListener("click", function () {
+document.querySelector("#inner-form-8 .previ-btn").addEventListener("click", function () {
     document.querySelector("#inner-form-8").style.display = "none";
     document.querySelector("#inner-form-7").style.display = "initial";
   });
+
+
